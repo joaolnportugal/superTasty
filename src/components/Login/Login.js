c7b5fadbe88c60169f4b2f53530d539b8d19ca41
joaolnportugal <joaolnportugal95@gmail.com>
2021-10-12 11:46:27 +0100

    login frontend done, moving on the authentication process

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = (e) => {
+    e.preventDefault();
+
+    //firebase login magic going on
+  };
+  const register = (e) => {
+    e.preventDefault();
+    //firebase register witchcraft
+  };
   return (
     <div className="login">
       <Link to="/">
-        <img className="loginLogo" src="https://dl.dropboxusercontent.com/s/4snbyg1ko6jm5hr/super%20tasty%20yellow%20logo.jpg?dl=0" />
+        <img
+          className="loginLogo"
+          src="https://dl.dropboxusercontent.com/s/4snbyg1ko6jm5hr/super%20tasty%20yellow%20logo.jpg?dl=0"
+        />
       </Link>
+      <div className="loginContainer">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" onClick={signIn} className="signinButton">
+            Sign In
+          </button>
+        </form>
+        <p>
+          By signing-in you agree to Super Tasty's conditions of Use & Sale.
+        </p>
+        <button onClick={register} className="loginRegistertButton">
+          Create your Super Tasty Account
+        </button>
+      </div>
     </div>
   );
 }
