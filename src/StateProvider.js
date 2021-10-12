1e27cb2bf317ac9bc5cced979d3aa59eebf932f5
joaolnportugal <joaolnportugal95@gmail.com>
2021-10-12 03:31:17 +0100

    reducer and state provider added, this time it's working so far

---
+++ src/StateProvider.js
@@ -0,0 +1,14 @@
+import React, { createContext, useContext, useReducer } from "react";
+
+//prepares data layer
+export const StateContext = createContext();
+
+//wraps the app and provides the data layer
+export const StateProvider = ({ reducer, initialState, children }) => (
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateContext.Provider>
+);
+
+//pull info from data layer
+export const useStateValue = () => useContext(StateContext);
